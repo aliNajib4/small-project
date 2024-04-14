0bfab0249ab2e44d9fe1af640de1b96016f4a35f
aliNajibHegazy <alinajibhegazy@gmail.com>
2024-04-14 16:49:58 +0200

    add scroll indicator

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import LoadMore from "./components/load_more/LoadMore";
 import TreeView from "./components/tree_view/TreeView";
 import QrCode from "./components/qr_code/QrCode";
 import DarkMode from "./components/dark_mode/DarkMode";
+import ScrollIndicator from "./components/scroll_indicator/ScrollIndicator";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route path="/tree-view" element={<TreeView />} />
           <Route path="/qr-code" element={<QrCode />} />
           <Route path="/dark-mode" element={<DarkMode />} />
+          <Route path="/scroll-indicator" element={<ScrollIndicator />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
---
+++ src/components/scroll_indicator/ScrollIndicator.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef, useState } from "react";
+import useFetch from "../../helpers/hooks/useFetch";
+
+const ScrollIndicator = () => {
+  const { data } = useFetch(
+    "https://dummyjson.com/products?limit=100&select=title",
+  );
+  const main = useRef(null);
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const height = main.current.offsetHeight - (window?.innerHeight || 0);
+      const scroll = window.scrollY;
+
+      setValue((scroll / height) * 100);
+    });
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+  return (
+    <>
+      <div
+        ref={main}
+        className="scroll-indicator absolute top-0 min-h-screen w-screen bg-cyan-300"
+      >
+        <header className="header center sticky left-0 top-0 w-screen flex-col bg-green-400 py-3">
+          <h1 className="mb-3 text-2xl font-bold ">ScrollIndicator</h1>
+          <progress value={value} max={100} className="h-2 w-full" />
+        </header>
+        <main>
+          <ul className="list m-5 list-decimal p-5">
+            {data?.products?.map(({ title, id }) => (
+              <li key={id}>{title}</li>
+            ))}
+          </ul>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default ScrollIndicator;
