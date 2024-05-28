afea596e6e0aa318cbaac5e4630f6b01ec55cc95
aliNajibHegazy <alinajibhegazy@gmail.com>
2024-05-28 08:29:26 +0300

    add home page

---
+++ src/components/home/Card.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const Card = ({ img, title, desc, URL }) => {
+  return (
+    <Link to={URL ? URL : "/"}>
+      <div
+        className="group relative flex h-44 flex-col justify-end overflow-hidden rounded-lg border bg-gray-100 bg-cover bg-center p-3 shadow-md duration-200 before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-[linear-gradient(180deg,transparent_50%,rgba(0,0,0,.25)_100%)] before:opacity-50 before:duration-200 hover:scale-110 hover:shadow-sm hover:before:opacity-25 dark:bg-neutral-700"
+        style={{ backgroundImage: `url(${img})` }}
+      >
+        <div className="relative z-10 p-2 duration-200 bg-white opacity-0 rounded-xl group-hover:opacity-65">
+          <h3 className="font-bold capitalize">{title}</h3>
+          <p className="ml-2 text-xs">{desc && desc}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+Card.propTypes = {
+  img: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string,
+  URL: PropTypes.string.isRequired,
+};
+
+export default Card;
---
+++ src/components/home/data.js
@@ -0,0 +1,107 @@
+import imageAccordion from "./images/accordion.png";
+import imageLoadMore from "./images/load-more.png";
+import imageQrCode from "./images/qr-code.png";
+import imageRandomColor from "./images/random-color.png";
+import imageStarRating from "./images/star-rating.png";
+import imageTreeView from "./images/tree-view.png";
+
+const data = [
+  {
+    name: "accordion",
+    path: "/accordion",
+    decsription: "this is accordion",
+    image: imageAccordion,
+  },
+  {
+    name: "random color",
+    path: "/random-color",
+    decsription: "this is random color",
+    image: imageRandomColor,
+  },
+  {
+    name: "star rating",
+    path: "/star-rating",
+    decsription: "this is star rating",
+    image: imageStarRating,
+  },
+  {
+    name: "sliders",
+    path: "/sliders",
+    decsription: "this is sliders",
+  },
+  {
+    name: "load more",
+    path: "/load-more",
+    decsription: "this is load more",
+    image: imageLoadMore,
+  },
+  {
+    name: "tree view",
+    path: "/tree-view",
+    decsription: "this is tree view",
+    image: imageTreeView,
+  },
+  {
+    name: "qr code",
+    path: "/qr-code",
+    decsription: "this is qr code",
+    image: imageQrCode,
+  },
+  {
+    name: "dark mode",
+    path: "/dark-mode",
+    decsription: "this is dark mode",
+  },
+  {
+    name: "scroll indicator",
+    path: "/scroll-indicator",
+    decsription: "this is scroll indicator",
+  },
+  {
+    name: "tabs",
+    path: "/tabs",
+    decsription: "this is tabs",
+  },
+  {
+    name: "popup",
+    path: "/popup",
+    decsription: "this is popup",
+  },
+  {
+    name: "github profile",
+    path: "/github-profile",
+    decsription: "this is github profile",
+  },
+  {
+    name: "auto complete",
+    path: "/auto-complete",
+    decsription: "this is auto complete",
+  },
+  {
+    name: "tic tac toe",
+    path: "/tic-tac-toe",
+    decsription: "this is tic tac toe",
+  },
+  {
+    name: "scroll to top and bottom",
+    path: "/scroll-to-top-and-bottom",
+    decsription: "this is scroll to top and bottom",
+  },
+  {
+    name: "weather",
+    path: "/weather",
+    decsription: "this is weather",
+  },
+  {
+    name: "timer down",
+    path: "/timer-down",
+    decsription: "this is timer down",
+  },
+  {
+    name: "parallax",
+    path: "/parallax",
+    decsription: "this is parallax",
+  },
+];
+
+export default data;
