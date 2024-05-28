1f713e578fe7c224d253188ad4649d08e128e985
aliNajibHegazy <alinajibhegazy@gmail.com>
2024-05-28 08:29:26 +0300

    add home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,20 @@
+import data from "./data";
+import Card from "./Card";
+
 const Home = () => {
+  console.log(data);
   return (
-    <>
-      <p>Home</p>
-    </>
+    <div className="m-8 grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+      {data.map(({ name, decsription, path, image }) => (
+        <Card
+          key={name}
+          title={name}
+          desc={decsription}
+          URL={path}
+          img={image}
+        />
+      ))}
+    </div>
   );
 };
 
---
+++ src/components/home/data.js
@@ -0,0 +1,128 @@
+import imageAccordion from "./images/accordion.png";
+
+const data = [
+  {
+    name: "accordion",
+    path: "/accordion",
+    decsription: "this is accordion",
+    image: imageAccordion,
+  },
+  {
+    name: "random color",
+    path: "/random-color",
+    decsription: "this is random color",
+  },
+  {
+    name: "star rating",
+    path: "/star-rating",
+    decsription: "this is star rating",
+  },
+  {
+    name: "sliders",
+    path: "/sliders",
+    decsription: "this is sliders",
+  },
+  {
+    name: "load more",
+    path: "/load-more",
+    decsription: "this is load more",
+  },
+  {
+    name: "tree view",
+    path: "/ tree-view",
+    decsription: "this is tree view",
+  },
+  {
+    name: "qr code",
+    path: "/qr-code",
+    decsription: "this is qr code",
+  },
+  {
+    name: "dark mode",
+    path: "/dark-mode",
+    decsription: "this is dark mode",
+  },
+  {
+    name: "scroll indicator",
+    path: "/scroll-indicator",
+    decsription: "this is scroll indicator",
+  },
+  {
+    name: "tabs",
+    path: "/tabs",
+    decsription: "this is tabs",
+  },
+  {
+    name: "popup",
+    path: "/popup",
+    decsription: "this is popup",
+  },
+  {
+    name: "github profile",
+    path: "/github-profile",
+    decsription: "this is github profile",
+  },
+  {
+    name: "auto complete",
+    path: "/auto-complete",
+    decsription: "this is auto complete",
+  },
+  {
+    name: "tic tac toe",
+    path: "/tic-tac-toe",
+    decsription: "this is tic tac toe",
+  },
+  {
+    name: "scroll to top and bottom",
+    path: "/scroll-to-top-and-bottom",
+    decsription: "this is scroll to top and bottom",
+  },
+  {
+    name: "weather",
+    path: "/weather",
+    decsription: "this is weather",
+  },
+  {
+    name: "timer down",
+    path: "/timer-down",
+    decsription: "this is timer down",
+  },
+  {
+    name: "parallax",
+    path: "/parallax",
+    decsription: "this is parallax",
+  },
+  //
+  {
+    name: "weather",
+    path: "/weather",
+    decsription: "this is weather",
+  },
+  {
+    name: "timer down",
+    path: "/timer-down",
+    decsription: "this is timer down",
+  },
+  {
+    name: "parallax",
+    path: "/parallax",
+    decsription: "this is parallax",
+  },
+  {
+    name: "weather",
+    path: "/weather",
+    decsription: "this is weather",
+  },
+  {
+    name: "timer down",
+    path: "/timer-down",
+    decsription: "this is timer down",
+  },
+  {
+    name: "parallax",
+    path: "/parallax",
+    decsription: "this is parallax",
+  },
+];
+
+export default data;
